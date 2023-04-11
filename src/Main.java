bba0d4b5061ddbd85d542bc8702d4d0e715ef020
Nicolas Melgar <elnico521@hotmail.com>
2023-04-11 06:56:27 -0300

    ejercicio8

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        Persona persona = new Persona(); 
+        persona.setterEdad1(43);
+
+        int edad = persona.getterEdad();
+        System.out.println(edad);
+
+        persona.setterTel(2944);
+        int tel = persona.getterTel();
+        System.out.println(tel);
+
+        persona.setterName("Nicolas");
+        String name = persona.getterName();
+        System.out.println(name);
+
+       
+        
+    }
+
+
+}
+
+class Persona{
+    private int edad; 
+    private int tel;
+    private String name;    
+
+    /*setter */
+public void setterEdad1(int valor){
+    this.edad = valor;
+}
+
+public void setterTel(int valor){
+    this.tel = valor;
+}
+
+public void setterName(String valor){
+    this.name = valor;
+}
+
+    /*getter */
+public int getterEdad(){
+    return this.edad;
+}
+
+public int getterTel(){
+    return this.tel;
+}
+
+public String getterName(){
+    return this.name;
+}
+
+}
